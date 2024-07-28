68b042ef527ed66387290220d2f5c563b0133eda
Joanna Cepuch <asiacepuch1@gmail.com>
2024-07-28 20:43:07 +0200

    changed from http to https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   api_key: '3cdae1460b895fb239ca842d56655292',
-  http_url: 'http://api.openweathermap.org/data/2.5',
-  pictures_url: ' http://openweathermap.org/img/wn/',
+  http_url: 'https://api.openweathermap.org/data/2.5',
+  pictures_url: ' https://openweathermap.org/img/wn/',
 };
